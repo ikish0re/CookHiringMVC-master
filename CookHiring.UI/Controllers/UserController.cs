57b7b38d0170cf4ab19184060e1fa35e2b1575eb
ikish0re <73517199+ikish0re@users.noreply.github.com>
2022-06-26 10:47:06 +0530

    Update UserController.cs

--- CookHiring.UI/Controllers/UserController.cs
+++ CookHiring.UI/Controllers/UserController.cs
@@ -56,6 +56,105 @@ namespace CookHiring.UI.Controllers
             userRepository.DeleteUser(id);
             return RedirectToAction("Index");
         }
+        public ActionResult Index()
+
+        {
+
+            var users = db.Users.Include(u => u.jobSeekerProfile);
+
+            return View(users.ToList());
+
+        }
+
+
+
+        // GET: Users/Details/5
+
+        public ActionResult Details(int? id)
+
+        {
+
+            if (id == null)
+
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            User user = db.Users.Find(id);
+
+            if (user == null)
+
+            {
+
+                return HttpNotFound();
+
+            }
+
+            return View(user);
+
+        }
+
+        // GET: Users/Edit/5
+
+        public ActionResult Edit(int? id)
+
+        {
+
+            if (id == null)
+
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            User user = db.Users.Find(id);
+
+            if (user == null)
+
+            {
+
+                return HttpNotFound();
+
+            }
+
+            ViewBag.id = new SelectList(db.jobSeekerProfiles, "jobSeekerId", "name", user.id);
+
+            return View(user);
+
+        }
+
+
+
+        // POST: Users/Edit/5
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit([Bind(Include = "id,email,password,mobileNumber,userRole")] User user)
+
+        {
+
+            if (ModelState.IsValid)
+
+            {
+
+                db.Entry(user).State = EntityState.Modified;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            ViewBag.id = new SelectList(db.jobSeekerProfiles, "jobSeekerId", "name", user.id);
+
+            return View(user);
+
+        }
 
     }
 }
